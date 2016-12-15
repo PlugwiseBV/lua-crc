150fc7d1481b149f0d215fee1c9e89573ea04677
Jacko Dirks <jacko.dirks@gmail.com>
2016-12-15 15:01:48 +0100

    Adds a test for the Xmodem CRC16

--- test.lua
+++ test.lua
@@ -8,6 +8,13 @@ ARCTestTable = {    ["AA"]          = 0x60F0,
                     ["3VrgJLJEGD"]  = 0xC29C
                 }
 
+XMODEMtesttable = { ["AA"]          = 0x6618,
+                    ["1AFXJbsJNW"]  = 0xCF5C,
+                    ["ILAiD5XOlJ"]  = 0xB1AD,
+                    ["QoCTLeRKKm"]  = 0xBE06,
+                    ["3VrgJLJEGD"]  = 0xC52F
+                }
+
 crc = crc16.new("ARC")
 for k,v in pairs(ARCTestTable) do
     -- Compute test
@@ -21,3 +28,16 @@ for k,v in pairs(ARCTestTable) do
     assert(sum == v, string.format("ARC CRC16 add function failed on string %s. Expected: %X, got: %X\n", k, v, sum))
 end
 io.write("CRC16 ARC test succesfull\n")
+crc = crc16.new("XMODEM")
+for k,v in pairs(XMODEMtesttable) do
+    -- Compute test
+    n = crc:compute(k)
+    assert(n == v, string.format("XMODEM CRC16 compute function failed on string %s. Expected: %X, got: %X\n", k, v, n))
+    -- Add test
+    sum = 0
+    for c in k:gmatch"." do
+        sum = crc:add(sum, c)
+    end
+    assert(sum == v, string.format("XMODEM CRC16 add function failed on string %s. Expected: %X, got: %X\n", k, v, sum))
+end
+io.write("CRC16 XMODEM test succesfull\n")
