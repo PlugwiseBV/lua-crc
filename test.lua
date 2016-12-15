124407aee44421d2562fa7ac33a3563f2bd2b5b0
Jacko Dirks <jacko.dirks@gmail.com>
2016-12-15 14:31:54 +0100

    Adds a test for the add function of ARC

--- test.lua
+++ test.lua
@@ -10,7 +10,14 @@ ARCTestTable = {    ["AA"]          = 0x60F0,
 
 crc = crc16.new("ARC")
 for k,v in pairs(ARCTestTable) do
+    -- Compute test
     n = crc:compute(k)
-    assert(n == v, string.format("ARC CRC16 failed on string %s. Expected: %X, got: %X", k, v, n))
+    assert(n == v, string.format("ARC CRC16 compute function failed on string %s. Expected: %X, got: %X\n", k, v, n))
+    -- Add test
+    sum = 0
+    for c in k:gmatch"." do
+        sum = crc:add(sum, c)
+    end
+    assert(sum == v, string.format("ARC CRC16 add function failed on string %s. Expected: %X, got: %X\n", k, v, sum))
 end
 io.write("CRC16 ARC test succesfull\n")
