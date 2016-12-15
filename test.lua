be5f107eaefca56091fc8837f2bb9d3c52d22206
Jacko Dirks <jacko.dirks@gmail.com>
2016-12-15 12:23:43 +0100

    Creates a testsuite for CRC16

--- test.lua
+++ test.lua
@@ -1,7 +1,16 @@
 #! /usr/bin/env lua5.1
 local crc16 = require "crc16"
 
-print(crc16)
+ARCTestTable = {    ["AA"]          = 0x60F0,
+                    ["1AFXJbsJNW"]  = 0x1888,
+                    ["ILAiD5XOlJ"]  = 0xC287,
+                    ["QoCTLeRKKm"]  = 0x213F,
+                    ["3VrgJLJEGD"]  = 0xC29C
+                }
+
 crc = crc16.new("ARC")
-print(crc:compute("lalala"))
-print(crc:compute("lalal"))
+for k,v in pairs(ARCTestTable) do
+    n = crc:compute(k)
+    assert(n == v, string.format("ARC CRC16 failed on string %s. Expected: %X, got: %X", k, v, n))
+end
+io.write("CRC16 ARC test succesfull\n")
